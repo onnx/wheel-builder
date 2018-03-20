52d272b5c688146334399b6c0e9ff35b9e602823
Raymond Yang <zihao.yang@microsoft.com>
2018-03-19 17:49:05 -0700

    Fix path

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,6 +4,6 @@ script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[
 source "${script_path%/*}/setup.sh"
 
 wheel_builder="$PWD"
-cd "${wheel_builder}\onnx"
+cd "${wheel_builder}/onnx"
 time ONNX_NAMESPACE=ONNX_NAMESPACE_FOO_BAR_FOR_CI python setup.py bdist_wheel --universal --dist-dir .
 find . -maxdepth 1 -name *.whl -exec pip install {} \;
