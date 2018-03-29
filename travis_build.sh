f36d32d3a937192e395d139a40cb6072d0675ad9
Raymond Yang <zihao.yang@microsoft.com>
2018-03-29 14:36:20 -0700

    Delete unused scripts

--- travis_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-script_path=$(python -c "import os; import sys; print(os.path.realpath(sys.argv[1]))" "${BASH_SOURCE[0]}")
-source "${script_path%/*}/.travis/before_install.sh"
-source "${script_path%/*}/.travis/install.sh"
