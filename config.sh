6f61be6e38d09741d0d1159e374ee96fddc32cff
Raymond Yang <zihao.yang@microsoft.com>
2018-05-24 15:45:50 -0700

    Remove parameters for debugging

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 function build_wheel {
     build_libs
     export ONNX_ML=1
-    time ONNX_NAMESPACE=ONNX_NAMESPACE build_bdist_wheel :all: --global-option build --global-option --debug $@
+    time ONNX_NAMESPACE=ONNX_NAMESPACE build_bdist_wheel $@
 }
 
 function build_libs {
