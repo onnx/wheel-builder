3d3a34dfc6412a0bd0c95ae042b3b9854099b13e
Raymond Yang <zihao.yang@microsoft.com>
2018-08-30 17:00:57 -0700

    try new pipeline on 1.2.2

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 function build_wheel {
     build_libs
     export ONNX_ML=1
-    time ONNX_NAMESPACE=ONNX_NAMESPACE build_bdist_wheel $@
+    time ONNX_NAMESPACE=ONNX_REL_1_3 build_bdist_wheel $@
 }
 
 function build_libs {
